a290a741ddac860fcc5d2167eefa0200946d7269
Andris Jansons <andris.jansons@gmail.com>
2016-09-16 21:43:53 +0300

    Push state

--- src/js/MyApp.js
+++ src/js/MyApp.js
@@ -2,6 +2,15 @@ var MyApp = (function () {
     var public = {};
 
 
+    public.init = function(){
+        $('#push-state').bind('click', perform);
+
+        function perform(){
+            console.log('Push the state!');
+            var clientId = Math.random();
+            window.history.pushState({}, '', '/clients/'+clientId);
+        }
+    };
 
     return public;
 })();
